6eb2baa88da2b7b1a635703fe7057235647a106a
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-05-26 12:15:46 +0700

    CRUD data

--- ecommerce-backend/controllers/productController.js
+++ ecommerce-backend/controllers/productController.js
@@ -28,6 +28,64 @@ const createProduct = async (req, res) => {
   };
 }
 
-module.exports = {
-  createProduct
+// Get all products
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({}); // Fetch all products from the database
+    res.status(200).json(products); // Return the products in the response  
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error while fetching product details' });
+  }
 }
+
+// Get one product following by id
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    !product ? res.status(404).json({ message: 'Product not found '}) : res.status(200).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error while fetching product details' });
+  }
+}
+
+// Update product information
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true} // Return new value after modify and check the validation of the data
+    );
+
+    !product 
+    ? res.status(404).json({ message: 'Product not found' }) // If product not found, return 404, Example when access the product with wrong id
+    : res.status(200).json(product); // If product found, return the updated product
+
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: 'Server error while updating product' }); // Server don't understand the data, for example when send string to price field, it will return 400 error
+  }
+}
+
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    !product
+    ? res.status(404).json({ message: 'Product not found' }) // If product not found, return 404
+    : res.status(200).json({ message: 'Product deleted successfully'}); // If product found, return success message
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error while deleting product' });
+  }
+}
+
+module.exports = {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct
+};
+
--- ecommerce-backend/routes/productRoutes.js
+++ ecommerce-backend/routes/productRoutes.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct } = require('../controllers/productController');
+const { createProduct,
+        getProducts,
+        getProductById,
+        updateProduct,
+        deleteProduct
+ } = require('../controllers/productController');
 
 // Route to create a new product
 router.post('/', createProduct);
+
+// Route to get all products
+router.get('/', getProducts);
+
+// Route to get a single product by ID
+router.get('/:id', getProductById);
+
+// Route to update a product
+router.put('/:id', updateProduct);
+
+// Route to delete a product
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
