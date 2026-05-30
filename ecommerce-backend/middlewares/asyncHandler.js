a612a7812945147bf421a0dbf42b36a8fb691f1e
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-05-30 15:05:30 +0700

    Convert configuration from js to ts

--- ecommerce-backend/middlewares/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next); // The function Promise.resolve will run the function fn(req, res, next) be called
-  // and return one value that you want. If it call one of the another promise return promise be called it.
-}
-
-module.exports = asyncHandler;
---
+++ ecommerce-backend/src/middlewares/asyncHandler.ts
@@ -0,0 +1,10 @@
+/**
+ * @param {import('express').RequestHandler} fn The async function to wrap.
+ * @returns {import('express').RequestHandler}
+ */
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next); // The function Promise.resolve will run the function fn(req, res, next) be called
+  // and return one value that you want. If it call one of the another promise return promise be called it.
+}
+
+module.exports = asyncHandler;
