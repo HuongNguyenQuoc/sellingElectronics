5f1ce8b00ed11094f670c4c0813f8408444fc46a
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-06-08 14:59:35 +0700

    Middleware for authorization admin

--- ecommerce-backend/src/middlewares/Role.middleware.ts
+++ ecommerce-backend/src/middlewares/Role.middleware.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Response } from 'express';
 import { AuthRequest } from './authMiddleware';
+import { AppError } from '../common/exceptions/AppError';
 
 export const restrictTo = (...roles: string[]) => {
-  return (req: AuthRequest, res: Response, next: NextFunction) => {
+  return (req: AuthRequest, _res: Response, next: NextFunction) => {
+    if (!req.user) return next(new AppError(401, 'Not authenticated.'))
     if (!roles.includes(req.user.role)) {
-      res.status(403);
-      throw new Error('Forbidden! You do not have permission.');
+      return next(new AppError(403, 'Forbidden! You do not have permissions.'));
     }
-    next();
+    return next();
   };
 };
