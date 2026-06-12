9730441b98321181e1622f30bcdb789a53756ad2
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-06-13 00:42:25 +0700

    Make a slight adjustment to the websocket section.

--- ecommerce-backend/src/repositories/message.repository.ts
+++ ecommerce-backend/src/repositories/message.repository.ts
@@ -1,18 +1,11 @@
 import { IMessage, Message } from '../models/Message';
 
 export class MessageRepository {
-<<<<<<< HEAD
-    async createMessage(data: IMessage) {
-        return Message.create(data);
-    }
-
-    async getAllMessages(roomId:string): Promise<IMessage[]> {
-        return Message.find({conversationId: roomId});
-    }
-}
-=======
   async createMessage(data: IMessage) {
     return Message.create(data);
   }
+
+  async getAllMessages(roomId: string): Promise<IMessage[]> {
+    return Message.find({ conversationId: roomId });
+  }
 }
->>>>>>> bb2dd1b (The basic features of the shopping cart on Fe and Be are now complete)
--- ecommerce-backend/src/services/message.service.ts
+++ ecommerce-backend/src/services/message.service.ts
@@ -1,33 +1,28 @@
-<<<<<<< HEAD
-import { SendMessageDto } from '../dtos_data-transfer-object/message.dto';
-import { MessageRepository } from '../repositories/message.repository';
-=======
 import { SendMessageDto } from '../dtos/message.dto';
->>>>>>> bb2dd1b (The basic features of the shopping cart on Fe and Be are now complete)
 import { IMessage } from '../models/Message';
 import { MessageRepository } from '../repositories/message.repository';
 
 const messageRepository = new MessageRepository();
-<<<<<<< HEAD
-export const sendMessageService = async (data: IMessage) => {
-    return messageRepository.createMessage(data);
-}
 
-export const getAllMessagesService = async(roomId:string) => {
-    return messageRepository.getAllMessages(roomId)
-}
-=======
-export const sendMessageService = async (dto: SendMessageDto) => {
-  const ConversationId = dto.receiverId === 'admin' ? dto.senderId : dto.receiverId;
+export const sendMessageService = async (dto: SendMessageDto | IMessage) => {
+  const conversationId =
+    'conversationId' in dto && dto.conversationId
+      ? dto.conversationId
+      : dto.receiverId === 'admin'
+        ? dto.senderId
+        : dto.receiverId;
+
   const message: IMessage = {
-    conversationId: ConversationId,
+    conversationId,
     senderId: dto.senderId,
     receiverId: dto.receiverId,
     content: dto.content,
-    type: dto.type, // "ask", "shipped"
-    productId: dto.productId,
     orderData: dto.orderData,
   };
+
   return messageRepository.createMessage(message);
 };
->>>>>>> bb2dd1b (The basic features of the shopping cart on Fe and Be are now complete)
+
+export const getAllMessagesService = async (roomId: string) => {
+  return messageRepository.getAllMessages(roomId);
+};
